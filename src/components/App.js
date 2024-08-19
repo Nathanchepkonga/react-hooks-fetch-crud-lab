67cec87fc76140c142d36d83efcab56ea0182f8f
Nathanchepkonga <66026997+Nathanchepkonga@users.noreply.github.com>
2024-08-19 09:35:51 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,16 @@
-import React, { useState, useEffect } from "react";
-import TaskList from "./TaskList";
-import TaskForm from "./TaskForm";
+import React, { useState } from "react";
+import AdminNavBar from "./AdminNavBar";
+import QuestionForm from "./QuestionForm";
+import QuestionList from "./QuestionList";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/tasks")
-      .then((response) => response.json())
-      .then((data) => setTasks(data));
-  }, []);
-
-  const addTask = (newTask) => {
-    fetch("http://localhost:3000/tasks", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newTask),
-    })
-      .then((response) => response.json())
-      .then((data) => setTasks([...tasks, data]));
-  };
-
-  const deleteTask = (id) => {
-    fetch(`http://localhost:3000/tasks/${id}`, {
-      method: "DELETE",
-    }).then(() => setTasks(tasks.filter((task) => task.id !== id)));
-  };
+  const [page, setPage] = useState("List");
 
   return (
-    <div className="App">
-      <h1>Task Manager</h1>
-      <TaskForm addTask={addTask} />
-      <TaskList tasks={tasks} deleteTask={deleteTask} />
-    </div>
+    <main>
+      <AdminNavBar onChangePage={setPage} />
+      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+    </main>
   );
 }
 
